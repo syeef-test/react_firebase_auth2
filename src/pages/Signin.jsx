b9b2a43d96a7811b2cc89825a433aaf6c56213a5
syeef <syeefislam@yahoo.com>
2024-04-08 19:17:13 +0530

    signin succesful

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,7 +1,71 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 function Signin() {
-  return <div>SignIn</div>;
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+
+    const key = import.meta.env.VITE_FIREBASE_APP_ID;
+
+    try {
+      setLoading(true);
+      const response = await fetch(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${key}`,
+        {
+          method: "POST",
+          body: JSON.stringify({
+            email: email,
+            password: password,
+            returnSecureToken: true,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.error.message);
+      }
+      const data = await response.json();
+      console.log(data.idToken);
+
+      console.log("User signed in successfully!");
+    } catch (error) {
+      console.error("Error signing up:", error.message);
+      alert(error.message);
+    } finally {
+      setLoading(false);
+    }
+
+    emailRef.current.value = "";
+    passwordRef.current.value = "";
+  };
+
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" ref={emailRef} required />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" ref={passwordRef} required />
+        </div>
+        <button type="submit" disabled={loading}>
+          {loading ? "Signing In..." : "Sign In"}
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Signin;
